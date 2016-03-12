e67142ec0fd583d71655c5be25c97017866cb110
vgx32 <salekhnovich@gmail.com>
2016-03-11 18:32:00 -0800

    added a stub template for the SPA frontend & set up URL routing for its endpoint (/app/)

--- site_watcher/urls.py
+++ site_watcher/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^app/', include('alert_manager.urls')),
+
 ]
