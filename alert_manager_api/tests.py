13350c51d6146f0e1a02ad747e691a8aba4e2aab
vgx32 <salekhnovich@gmail.com>
2016-03-13 19:03:50 -0700

    stubbed out tests

--- alert_manager_api/tests.py
+++ alert_manager_api/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class AlertApiTests(TestCase):
+
+  def test_create_alert(self):
+    pass
+  
+  def test_create_with_required_fields(self):
+    pass
+
+  def test_get_alert(self):
+    pass
+
+  def test_edit_alert(self):
+    pass
+
+  def test_delete(self):
+    pass
+
+  def test_get_all_alerts(self):
+    pass
+
+  def test_run_alert_search(self):
+    pass
+
+  def test_get_match_results(self):
+    pass
+
+  def test_clear_results(self):
+    pass
+
+
+  def test_results_e2e(self):
+    print("TODO: think about what to do for this case")
+    pass
+  
