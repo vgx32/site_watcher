7983c4e6c6fe624e450bc2a6f11438bb8d839d58
vgx32 <salekhnovich@gmail.com>
2016-03-13 20:17:59 -0700

    added create_alert test case and exposed all url patterns to the world

--- alert_manager_api/tests.py
+++ alert_manager_api/tests.py
@@ -1,20 +1,48 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
 
 # Create your tests here.
 
-class AlertApiTests(TestCase):
+class AlertApiTests(APITestCase):
+
+  def setup(self):
+    self.testAlert = {
+      "root_url" : "www.example.com",
+      "scrape_level" : 3,
+      "search_terms" : ["domain", "example"],
+      "analysis_op" : "string_match",
+      "notification_type": "none"
+    }
+    
+
+  alerts_name = "alerts"
+  alert_name = "alert"
+  matchest_name = "matches"
+  match_name = "match"
 
   def test_create_alert(self):
-    pass
+    alert_endpoint = reverse(self.alerts_name)
+    r = self.client.get(alert_endpoint)
+    self.assertTrue(status.is_success(r.status_code))
+    self.assertEqual(r.data, [])
+
+    r = self.client.post(alert_endpoint, self.testAlert)
+    self.assertTrue(status.is_success(r.status_code))
+    self.assertEqual(r.data, self.testAlert)
   
   def test_create_with_required_fields(self):
-    pass
-
-  def test_get_alert(self):
+    print("TODO returning error when some fields missing")
     pass
 
   def test_edit_alert(self):
     pass
+
+  def test_alert_nonexistant(self):
+    print("TODO ")
 
   def test_delete(self):
     pass
--- alert_manager_api/urls.py
+++ alert_manager_api/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.root, name='api_root'),
-    # url(r'^alerts/$', views.root, name='alerts'),
-    # url(r'^alerts/(?P<alert_id>[0-9]+)/$', views.root, name='alert'),
-    # url(r'^matches/$', views.root, name='matches'),
-    # url(r'^matches/(?P<notification_id>[0-9]+)/$', views.root, name='match'),
+    url(r'^alerts/$', views.root, name='alerts'),
+    url(r'^alerts/(?P<alert_id>[0-9]+)/$', views.root, name='alert'),
+    url(r'^matches/$', views.root, name='matches'),
+    url(r'^matches/(?P<notification_id>[0-9]+)/$', views.root, name='match'),
     
 
 
